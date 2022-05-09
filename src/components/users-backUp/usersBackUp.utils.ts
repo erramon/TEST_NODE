bf9d8851fa7165de81caeaef9b7e46cd91b30ae2
Alejandro Exposito <portmeo@gmail.com>
2022-05-09 04:58:53 +0200

    refactor blocks

--- src/components/users-backUp/usersBackUp.utils.ts
+++ src/components/users-backUp/usersBackUp.utils.ts
@@ -14,20 +14,20 @@ class UsersBackUpUtils {
     async createBackup(apiUrl: string, api: string): Promise<boolean[]> {
         const path = `src/outputs/${api}`;
         const MAX_BLOCK_LINES = 999;
-        let isCreatedBlock: boolean[];
+        let isCreatedBlocks: boolean[];
 
         try {
             await handlerFilesUtil.cleanDirectory(path);
 
             const usersBackUpData = await usersBackUpService.getDataUsers(apiUrl);
 
-            isCreatedBlock = await this.createBlocks(usersBackUpData, path, MAX_BLOCK_LINES);
+            isCreatedBlocks = await this.createBlocks(usersBackUpData, path, MAX_BLOCK_LINES);
 
         } catch (err) {
             console.error(`Error create users backUp data url: ${apiUrl}`);
         }
 
-        return isCreatedBlock;
+        return isCreatedBlocks;
     }
 
     /**
@@ -38,30 +38,18 @@ class UsersBackUpUtils {
      * @returns {Promise<boolean[]>} A promises array that contains if blocks were created
      */
     createBlocks(dataUsers: BackUpResponse, path: string, maxLinesBlock: number): Promise<boolean[]> {
+        if (maxLinesBlock <= 0) throw 'Invalid max lines block';
+
         const promises: Promise<boolean>[] = [];
         const date = new Date();
-
         let block = 1;
-        let dataBlock: string[] = [];
-        
-        dataUsers.items.forEach((user: User, index: number) => {
-            
-            dataBlock.push(Object.values(user).toString());
 
-            if (dataBlock.length === maxLinesBlock) {
-                const fileName = `${path}/${CONSTANTS.MONTH_NAMES[date.getMonth()]}_backup_${block}.csv`;
-                promises.push(handlerFilesUtil.writeFile(fileName, dataBlock.join('\n')));
-                block++;
-                dataBlock = [];
-            }
-
-            if (index === (dataUsers.items.length - 1) && dataBlock.length) {
-                const fileName = `${path}/${CONSTANTS.MONTH_NAMES[date.getMonth()]}_backup_${block}.csv`;
-                promises.push(handlerFilesUtil.writeFile(fileName, dataBlock.join('\n')));
-                block++;
-            }
-
-        });
+        for (let i = 0; i < dataUsers.items.length; i += maxLinesBlock) {
+            const usersByBlock = dataUsers.items.slice(i, i + maxLinesBlock);
+            const dataBlock = usersByBlock.map((user: User) => Object.values(user).toString()).join('\n');
+            const fileName = `${path}/${CONSTANTS.MONTH_NAMES[date.getMonth()]}_backup_${block++}.csv`;
+            promises.push(handlerFilesUtil.writeFile(fileName, dataBlock));
+        }
 
         return Promise.all(promises);
     }
